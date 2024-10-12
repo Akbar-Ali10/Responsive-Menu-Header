8201d0b7710f9306e8e6022a26a126304e29ab51
Ali <baltimuhammad38@gmail.com>
2024-10-13 00:09:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+let icon = document.querySelector(".icon");
+let ul = document.querySelector("ul");
+
+
+icon.addEventListener("click", function(){
+    ul.classList.toggle("showData"); 
+    console.log(ul);
+    
+})
